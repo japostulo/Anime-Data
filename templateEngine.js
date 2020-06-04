81210e47d0e3b86a3b97da5f1ac628c8d71afec3
João Apostulo <japostulo@gmail.com>
2020-06-04 12:15:32 -0300

    Recriado o footer

--- templateEngine.js
+++ templateEngine.js
@@ -1,16 +1,23 @@
 var data = {
+    navBar:[
+      {
+        Item:'Home',
+        click:'toggleSearchBar(this)'
+      }
+    ],
+
     midiaFooter:[
       {
         link:"https://facebook.com.br",
-        icon:"fa fa-1x fa-facebook"
+        icon:"fa fa-inverse fa-1x fa-facebook"
       },
       {
         link:"https://instagram.com.br",
-        icon:"fa fa-1x fa-instagram"
+        icon:"fa fa-inverse fa-1x fa-instagram"
       },
       {
         link:"https://youtube.com.br",
-        icon:"fa fa-1x fa-youtube"
+        icon:"fa fa-inverse fa-1x fa-youtube"
       }
     ],
     menuFooter:[
@@ -21,26 +28,39 @@ var data = {
       {
         Item:"CONTATO",
         link:"#"
-      }
+      },
+
     ]
     };
     var html = "";
+
+    //Template Engine NAVBAR
+    var tplNavBar =  "{{ #navBar }}" +
+                     "<li class='nav-item'>" +
+                     "<a class='nav-link' href='#' id='btnHome' onclick='{{ click }}'>{{ Item }}</a>" +
+                     "</li>" +
+                     "{{ /navBar }}";
+    html = Mustache.render(tplNavBar, data);
+    if(document.getElementById("midiaFooter")){
+      document.getElementById("navBar").innerHTML = html;
+    }
+
     //Template Engine MIDIA FOOTER
     var tplMidia =  "{{ #midiaFooter }}" +
                     "<li class='border d-flex align-items-center justify-content-center m-1' style='width:2rem; height:2rem; text-decoration:none;'> <a href='{{link}}' class='{{ icon }}' target='_blank'> </a> </li>" +
                     "{{ /midiaFooter }}";
     html = Mustache.render(tplMidia, data);
+
     if(document.getElementById("midiaFooter")){
       document.getElementById("midiaFooter").innerHTML = html;
     }
 
-
-
     //Template Engine MENU FOOTER
     var tplMenuFooter = "{{ #menuFooter }}"+
-                        "<li class='d-flex ml-4 font-weight-bold'> <a href='{{ link }}' style='color:black; text-decoration:none;'> {{ Item }} </a> </li>"+
+                        "<li class='d-flex ml-4 font-weight-bold'> <a href='{{ link }}' style='color:white; text-decoration:none;'> {{ Item }} </a> </li>"+
                         "{{ /menuFooter }}";
     html = Mustache.render(tplMenuFooter, data);
+
     if(document.getElementById("menuFooter")){
       document.getElementById("menuFooter").innerHTML = html;
     }
